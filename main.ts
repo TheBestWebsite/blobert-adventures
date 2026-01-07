731c828b0cd105bbac34c44034a5d7252c5b5179
TheBestWebsite <164817940+TheBestWebsite@users.noreply.github.com>
2026-01-07 09:16:21 -0800

    Clean up initial assets

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 function CreatePlayer () {
-    mySprite = sprites.create(img`
+    Blobert = sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
@@ -18,7 +18,7 @@ function CreatePlayer () {
         . 7 7 7 7 7 7 7 7 7 7 7 7 b b . 
         `, SpriteKind.Player)
     animation.runImageAnimation(
-    mySprite,
+    Blobert,
     [img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
@@ -167,11 +167,9 @@ function CreatePlayer () {
     100,
     true
     )
-    controller.moveSprite(mySprite)
-    scene.cameraFollowSprite(mySprite)
+    controller.moveSprite(Blobert)
+    scene.cameraFollowSprite(Blobert)
 }
-let mySprite: Sprite = null
+let Blobert: Sprite = null
 CreatePlayer()
-tiles.setCurrentTilemap(tilemap`level1`)
 music.play(music.createSong(hex`00b40004080c0106001c00010a006400f401640000040000000000000000000000000000000002ab0000001800012218003000012430004800012548006000012760006800021d2968007000021b2770007800021925780080000218248000880002182488009000021622900098000218249800a000021824a000a800021925a800b800021824b800c000021622c000d800021824d800e000021622e000f800021420f800100102121e10012401011b28014001011940014801011b48015801011d58016001011e600170010120700180010122`), music.PlaybackMode.LoopingInBackground)
-let list = [0, 1, 0]
