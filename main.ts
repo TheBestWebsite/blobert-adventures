f3e1daee345718ba12f299db004bbaece3aa20be
TheBestWebsite <164817940+TheBestWebsite@users.noreply.github.com>
2026-01-14 21:52:51 -0800

    Stuff

--- main.ts
+++ main.ts
@@ -180,6 +180,29 @@ function CreatePlayer () {
     true
     )
 }
+scene.onOverlapTile(SpriteKind.Player, assets.tile`signBoard`, function (sprite, location) {
+    if (SignNumber == 1) {
+        game.showLongText("These are coins, collect them as you go on!", DialogLayout.Bottom)
+        SignNumber = 2
+        tiles.placeOnTile(Blobert, location)
+        tiles.setTileAt(location, assets.tile`transparency16`)
+    } else if (SignNumber == 2) {
+        game.showLongText("These are Rocky walls, you can Wall Jump on them", DialogLayout.Bottom)
+        SignNumber = 3
+        tiles.placeOnTile(Blobert, location)
+        tiles.setTileAt(location, assets.tile`transparency16`)
+    } else if (SignNumber == 3) {
+        game.showLongText("This is red slime, it dosent let you move, and moves you to the left", DialogLayout.Bottom)
+        SignNumber = 4
+        tiles.placeOnTile(Blobert, location)
+        tiles.setTileAt(location, assets.tile`transparency16`)
+    } else {
+        game.showLongText("This is blue slime, red slime but to the right", DialogLayout.Bottom)
+        SignNumber = 5
+        tiles.placeOnTile(Blobert, location)
+        tiles.setTileAt(location, assets.tile`transparency16`)
+    }
+})
 function replaceSlime () {
     for (let value of tiles.getTilesByType(assets.tile`redSlime`)) {
         tiles.setTileAt(value, assets.tile`redSlime1`)
@@ -255,9 +278,6 @@ function startScene () {
     sprites.destroy(Title)
     sprites.destroy(toStart)
 }
-scene.onOverlapTile(SpriteKind.Player, assets.tile`transparency16`, function (sprite, location) {
-    game.setGameOverMessage(true, "You Win!")
-})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     sprites.destroy(otherSprite)
     info.changeLifeBy(-1)
@@ -273,10 +293,12 @@ let canWallJumpLeft = false
 let newLevel = 0
 let wallTiles: Image[] = []
 let Blobert: Sprite = null
+let SignNumber = 0
 let gameStart = false
 CreatePlayer()
 IndustryCredits()
 let level = 0
+SignNumber = 1
 startScene()
 music.stopAllSounds()
 music.play(music.createSong(hex`00b40004080c0106001c00010a006400f401640000040000000000000000000000000000000002ab0000001800012218003000012430004800012548006000012760006800021d2968007000021b2770007800021925780080000218248000880002182488009000021622900098000218249800a000021824a000a800021925a800b800021824b800c000021622c000d800021824d800e000021622e000f800021420f800100102121e10012401011b28014001011940014801011b48015801011d58016001011e600170010120700180010122`), music.PlaybackMode.LoopingInBackground)
@@ -292,6 +314,7 @@ let redSlime = [assets.tile`redSlime`, assets.tile`redSlime1`, assets.tile`redSl
 wallTiles = [sprites.builtin.forestTiles0, sprites.castle.tileGrass1]
 let portals = [assets.tile`portalWallJumpRight`]
 level = nextLevel(level)
+game.setGameOverMessage(true, "You Win!")
 game.onUpdate(function () {
     if (Blobert.isHittingTile(CollisionDirection.Right)) {
         canWallJumpLeft = true
